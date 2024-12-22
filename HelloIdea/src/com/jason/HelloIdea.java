79fa85a0db742722cf63ffa01001ff365402d757
jasonyell <65152013+jasonyell@users.noreply.github.com>
2024-12-23 01:32:31 +0800

    Add files via upload

---
+++ HelloIdea/src/com/jason/HelloIdea.java
@@ -0,0 +1,14 @@
+package com.jason;
+
+/**
+ * @program: HelloIdea
+ * @description
+ * @author: JasonYell
+ * @create: 2023-02-21 02:35
+ **/
+public class HelloIdea {
+
+    public static void main(String[] args) {
+        System.out.println("jason");
+    }
+}
